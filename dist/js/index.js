440f6b3a27340d4eb443e1b9ba1a1e853384d5da
Emilio Sanchez <milaroco3004@gmail.com>
2024-05-08 20:53:35 -0500

    Mejorando la interfaz para editar las publicaciones, e intentando implementar un temporizador para el tiempo de publicacion

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,16 @@
-const tiempoPost = document.body.getElementsByClassName('tiempo-post');
-tiempoPost.innerText = '10min';
+let contador = 0;
+document.getElementById("demo").innerHTML = contador + "min(s)";
+setInterval(function () {
+  contador++;
+  console.log("El contador ahora es: " + contador);
+  document.getElementById("demo").innerHTML = contador + "min(s)";
+}, 60000); // 60000 milisegundos equivalen a 1 minuto
+
+
+editBtnY = document.getElementById('editBtn1')
+editBtnN = document.getElementById('editBtn0')
+editCardBody = document.getElementById('editCardBody')
+
+editBtnY.addEventListener("click", () => {
+    editCardBody.innerHTML = "<h8 class='card-text'><strong>Selecciona la Imagen: </strong></h8>" + "<br>" + "<input type='file' id='imagen'>" + "<br>" + "<br>" + "<h8 class='card-text'><strong>Escribe el nuevo nombre y Fecha con el siguiente formato: </strong></h8>" + "<p class='card-text'>[Nombre del Modelo] [Fecha de modelo]</p>" + "<input type='text' id='titulo-fecha'></input>" + "<br>" + "<br>" + "<h9 class='card-text'><strong>Deseas cambiar la descripción del modelo?</strong></h9>" + "<br>" + "<input type='text' id='descripcion'></input>"
+});
