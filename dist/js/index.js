83e97765f7332577eb5c257e4e29c5789fb00682
Emilio Sanchez <milaroco3004@gmail.com>
2024-05-19 20:11:14 -0500

    pequeños cambios en el formato del codigo y la interfaz del modo de edición

--- dist/js/index.js
+++ dist/js/index.js
@@ -54,29 +54,153 @@ function mostrarPosts() {
                 </div>
             </div>
         `;
-
         post.innerHTML = postContent;
         contenedorPosts.appendChild(post);
     }
 }
-
-const editBtnY = document.getElementById("editBtn1");
-const editBtnN = document.getElementById("editBtn0");
 const editCardBody = document.getElementById("editCardBody");
-
-editBtnY.addEventListener("click", () => {
+const editBtnY = document.getElementById("editBtn1");
+function editarPosts() {
     editCardBody.innerHTML =
-        "<h8 class='card-text'><strong>Selecciona la Imagen: </strong></h8>" +
-        "<br>" +
-        "<input type='file' id='imagen'>" +
-        "<br>" +
-        "<br>" +
-        "<h8 class='card-text'><strong>Escribe el nuevo nombre y Fecha con el siguiente formato: </strong></h8>" +
-        "<p class='card-text'>[Nombre del Modelo] [Fecha de modelo]</p>" +
-        "<input type='text' id='titulo-fecha'></input>" +
-        "<br>" +
-        "<br>" +
-        "<h9 class='card-text'><strong>Deseas cambiar la descripción del modelo?</strong></h9>" +
-        "<br>" +
-        "<input type='text' id='descripcion'></input>";
-});
+        `<h8 class='card-text'><strong>Selecciona la Imagen: </strong></h8>
+        <br>
+        <input type='file' id='imagen'>
+        <br>
+        <br>
+        <h8 class='card-text'><strong>Escribe el nuevo nombre y Fecha con el siguiente formato: </strong></h8>
+        <p class='card-text'>[Nombre del Modelo] [Fecha de modelo]</p>
+        <input type='text' id='titulo-fecha'></input>
+        <br>
+        <br>
+        <h9 class='card-text'><strong>Ingresa la nueva descripción del modelo</strong></h9>
+        <br>
+        <input type='text' id='descripcion'></input>
+        <h5 class="card-text">¿Quieres salir del modo de edición?</h5>
+        <div class="btn-group">
+            <button onclick="salirEditMode()" id='editBtn1' type="button" class="btn btn-sm btn-outline-secondary">
+                Si
+            </button>
+            <button onclick="quedarseEditMode()" id='editBtn0' type="button" class="btn btn-sm btn-outline-secondary">
+                No
+            </button>
+        </div>`;
+}
+
+const editBtnN = document.getElementById("editBtn0");
+function noEditarPosts() {
+    editCardBody.innerHTML = `
+    <div id="editModalCarouselIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+            <button id="slide" type="button" data-bs-target="#editModalCarouselIndicators"
+                data-bs-slide-to="0" class="active" aria-current="true"
+                aria-label="Slide 1"></button>
+            <button id="slide" type="button" data-bs-target="#editModalCarouselIndicators"
+                data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button id="slide" type="button" data-bs-target="#viewModalCarouselIndicators"
+                data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button id="carousel-btn" class="carousel-control-prev" type="button"
+            data-bs-target="#editModalCarouselIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button id="carousel-btn" class="carousel-control-next" type="button"
+            data-bs-target="#editModalCarouselIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <br>
+    <h5 class="card-text">
+        <strong>[Nombre del Modelo] [Fecha de modelo]</strong>
+    </h5>
+    <p class="card-text">
+        Una breve descripción hablando sobre el modelo.
+        <div class="d-flex justify-content-between align-items-center">
+            <small class="text-body-secondary">9 mins</small>
+        </div>
+    </p>`
+}
+
+function quedarseEditMode() {
+    editCardBody.innerHTML =
+        `<h8 class='card-text'><strong>Selecciona la Imagen: </strong></h8>
+        <br>
+        <input type='file' id='imagen'>
+        <br>
+        <br>
+        <h8 class='card-text'><strong>Escribe el nuevo nombre y Fecha con el siguiente formato: </strong></h8>
+        <p class='card-text'>[Nombre del Modelo] [Fecha de modelo]</p>
+        <input type='text' id='titulo-fecha'></input>
+        <br>
+        <br>
+        <h9 class='card-text'><strong>Ingresa la nueva descripción del modelo</strong></h9>
+        <br>
+        <input type='text' id='descripcion'></input>`;
+}
+
+function salirEditMode() {
+    editCardBody.innerHTML =`
+    <div id="editModalCarouselIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+            <button id="slide" type="button" data-bs-target="#editModalCarouselIndicators"
+                data-bs-slide-to="0" class="active" aria-current="true"
+                aria-label="Slide 1"></button>
+            <button id="slide" type="button" data-bs-target="#editModalCarouselIndicators"
+                data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button id="slide" type="button" data-bs-target="#viewModalCarouselIndicators"
+                data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button id="carousel-btn" class="carousel-control-prev" type="button"
+            data-bs-target="#editModalCarouselIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button id="carousel-btn" class="carousel-control-next" type="button"
+            data-bs-target="#editModalCarouselIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <h5 class="card-text"><strong>¿Deseas editar la información del modelo?</strong></h5>
+    <div class="btn-group">
+        <button onclick="editarPosts()" id='editBtn1' type="button" class="btn btn-sm btn-outline-secondary">
+            Si
+        </button>
+        <button onclick="noEditarPosts()" id='editBtn0' type="button" class="btn btn-sm btn-outline-secondary">
+            No
+        </button>
+    </div>
+    <br>
+    <h5 class="card-text">
+        <strong>[Nombre del Modelo] [Fecha de modelo]</strong>
+    </h5>
+    <p class="card-text">
+        Una breve descripción hablando sobre el modelo.
+        <div class="d-flex justify-content-between align-items-center">
+            <small class="text-body-secondary">9 mins</small>
+        </div>
+    </p>`
+}
