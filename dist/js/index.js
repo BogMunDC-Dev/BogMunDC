7426bcc8fbb1d3803ce9487080d1675f9bd73b9f
ESH2007 <milaroco3004@gmail.com>
2024-08-28 21:27:01 -0500

    Arreglando un pequeño error de la interfaz relacionado con el filtro de los modelos

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,132 +1,133 @@
 const db = {
-    "modelos": [
-      {
-        "nombre": "RMUN XIX",
-        "institucion": "Colegio Rochester",
-        "Fecha": "29 y 30 de Enero",
-        "linkInstagram": "https://www.instagram.com/rochester.mun/",
-        "locacion": "Vereda Fusca, Chia, Cundinamarca, 250008"
-      },
-      {
-        "nombre": "CNGMUN XLII",
-        "institucion": "Colegio Nueva Granada",
-        "Fecha": "8, 9 y 10 de Febrero",
-        "linkInstagram": "https://www.instagram.com/cngmun2024/",
-        "locacion": "Cra 2 Este #70-20, Bogota"
-      },
-      {
-        "nombre": "MUNEB 2024",
-        "institucion": "Universidad del Bosque",
-        "Fecha": "15, 16, 17 y  18 de Febrero",
-        "linkInstagram": "https://www.instagram.com/stories/muneb_org/",
-        "locacion": "Ak. 9 #131a-2, Bogota"
-      },
-      {
-        "nombre": "CSVMUN III",
-        "institucion": "Colegio San Viator",
-        "Fecha": "22 y 23 de Febrero",
-        "linkInstagram": "https://www.instagram.com/csvmun_/",
-        "locacion": "Autopista Nte. #209-51, Suba, Bogota"
-      },
-      {
-        "nombre": "TVSMUN XVII",
-        "institucion": "Colegio The Victoria School",
-        "Fecha": "23, 24 y 25 de Febrero",
-        "linkInstagram": "https://www.instagram.com/tvsmun/",
-        "locacion": "Calle 215 No. 50-60, Suba, Bogota"
-      },
-      {
-        "nombre": "CLNMUN VI",
-        "institucion": "Colegio Los Nogales",
-        "Fecha": "8, 9 y 10 de Marzo",
-        "linkInstagram": "https://www.instagram.com/p/Ctjo3t3OQbz/",
-        "locacion": "Cl. 202 #5650, Bogota"
-      },
-      {
-        "nombre": "SACMUN XVIII",
-        "institucion": "Colegio San Carlos",
-        "Fecha": "21, 22 y 23 de Marzo",
-        "linkInstagram": "https://www.instagram.com/sacmuncsc/",
-        "locacion": "Cl. 192 #9 - 45, Bogota"
-      },
-      {
-        "nombre": "CUMUN",
-        "institucion": "Colegio Cumbres",
-        "Fecha": "10, 11 y 12 de Abril",
-        "linkInstagram": "https://www.instagram.com/cumun__/",
-        "locacion": "Autonor. Km. 6 Vereda Yerbabuena, Chia"
-      },
-      {
-        "nombre": "CSMUN X",
-        "institucion": "Colegio Bilingue Clermont",
-        "Fecha": "10, 11 y 12 de Abril",
-        "linkInstagram": "https://www.instagram.com/csmun_/",
-        "locacion": "Tv. 80 #213a-17, Bogota"
-      }
-    ]
+  modelos: [
+    {
+      nombre: "RMUN XIX",
+      institucion: "Colegio Rochester",
+      Fecha: "29 y 30 de Enero",
+      linkInstagram: "https://www.instagram.com/rochester.mun/",
+      locacion: "Vereda Fusca, Chia, Cundinamarca, 250008",
+    },
+    {
+      nombre: "CNGMUN XLII",
+      institucion: "Colegio Nueva Granada",
+      Fecha: "8, 9 y 10 de Febrero",
+      linkInstagram: "https://www.instagram.com/cngmun2024/",
+      locacion: "Cra 2 Este #70-20, Bogota",
+    },
+    {
+      nombre: "MUNEB 2024",
+      institucion: "Universidad del Bosque",
+      Fecha: "15, 16, 17 y  18 de Febrero",
+      linkInstagram: "https://www.instagram.com/stories/muneb_org/",
+      locacion: "Ak. 9 #131a-2, Bogota",
+    },
+    {
+      nombre: "CSVMUN III",
+      institucion: "Colegio San Viator",
+      Fecha: "22 y 23 de Febrero",
+      linkInstagram: "https://www.instagram.com/csvmun_/",
+      locacion: "Autopista Nte. #209-51, Suba, Bogota",
+    },
+    {
+      nombre: "TVSMUN XVII",
+      institucion: "Colegio The Victoria School",
+      Fecha: "23, 24 y 25 de Febrero",
+      linkInstagram: "https://www.instagram.com/tvsmun/",
+      locacion: "Calle 215 No. 50-60, Suba, Bogota",
+    },
+    {
+      nombre: "CLNMUN VI",
+      institucion: "Colegio Los Nogales",
+      Fecha: "8, 9 y 10 de Marzo",
+      linkInstagram: "https://www.instagram.com/p/Ctjo3t3OQbz/",
+      locacion: "Cl. 202 #5650, Bogota",
+    },
+    {
+      nombre: "SACMUN XVIII",
+      institucion: "Colegio San Carlos",
+      Fecha: "21, 22 y 23 de Marzo",
+      linkInstagram: "https://www.instagram.com/sacmuncsc/",
+      locacion: "Cl. 192 #9 - 45, Bogota",
+    },
+    {
+      nombre: "CUMUN",
+      institucion: "Colegio Cumbres",
+      Fecha: "10, 11 y 12 de Abril",
+      linkInstagram: "https://www.instagram.com/cumun__/",
+      locacion: "Autonor. Km. 6 Vereda Yerbabuena, Chia",
+    },
+    {
+      nombre: "CSMUN X",
+      institucion: "Colegio Bilingue Clermont",
+      Fecha: "10, 11 y 12 de Abril",
+      linkInstagram: "https://www.instagram.com/csmun_/",
+      locacion: "Tv. 80 #213a-17, Bogota",
+    },
+  ],
 };
 
 function mostrarPosts() {
-    const numeroPosts = document.getElementById('number-post').value;
-    const contenedorPosts = document.querySelector('.contenedor-posts');
-    contenedorPosts.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevos posts
+  const numeroPosts = document.getElementById("number-post").value;
+  const contenedorPosts = document.querySelector(".contenedor-posts");
+  contenedorPosts.innerHTML = ""; // Limpiar el contenedor antes de agregar nuevos posts
 
-    for (let i = 0; i < numeroPosts; i++) {
-        const post = document.createElement('div');
-        post.classList.add('post', 'col', 'id="post-model"'); // Add all provided classes
+  for (let i = 0; i < numeroPosts; i++) {
+    const post = document.createElement("div");
+    post.classList.add("post", "col", 'id="post-model"'); // Add all provided classes
 
-        const postContent = `
-            <div class="card shadow-sm">
-                <div id="carouselExampleIndicators${i}" class="carousel slide">
-                    <div class="carousel-indicators">
-                        <button id="slide" type="button" data-bs-target="#carouselExampleIndicators${i}"
-                            data-bs-slide-to="0" class="active" aria-current="true"
-                            aria-label="Slide 1"></button>
-                        <button id="slide" type="button" data-bs-target="#carouselExampleIndicators${i}"
-                            data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button id="slide" type="button" data-bs-target="#carouselExampleIndicators${i}"
-                            data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <button id="carousel-btn" class="carousel-control-prev" type="button"
-                        data-bs-target="#carouselExampleIndicators${i}" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button id="carousel-btn" class="carousel-control-next" type="button"
-                        data-bs-target="#carouselExampleIndicators${i}" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
+    const postContent = `
+    <div class="card shadow-sm">
+        <div id="carouselExampleIndicators${i}" class="carousel slide">
+            <div class="carousel-indicators">
+                <button id="slide" type="button" data-bs-target="#carouselExampleIndicators${i}"
+                    data-bs-slide-to="0" class="active" aria-current="true"
+                    aria-label="Slide 1"></button>
+                <button id="slide" type="button" data-bs-target="#carouselExampleIndicators${i}"
+                    data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button id="slide" type="button" data-bs-target="#carouselExampleIndicators${i}"
+                    data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
                 </div>
-                <div class="card-body">
-                    <h5 class="card-text"><strong>${db.modelos[i].nombre} ${db.modelos[i].Fecha}</strong></h5>
-                    <p class="card-text"> Una breve descripción hablando sobre el modelo.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#ViewModal">View</button>
-                        </div>
-                        <small class="text-body-secondary">9 mins</small>
-                    </div>
+                <div class="carousel-item">
+                    <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="img/Imagen_ONU_Prueba.png" class="d-block w-100" alt="...">
                 </div>
             </div>
-        `;
-        post.innerHTML = postContent;
-        contenedorPosts.appendChild(post);
-    }
+            <button id="carousel-btn" class="carousel-control-prev" type="button"
+                data-bs-target="#carouselExampleIndicators${i}" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button id="carousel-btn" class="carousel-control-next" type="button"
+                data-bs-target="#carouselExampleIndicators${i}" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <div class="card-body">
+            <h5 class="card-text"><strong>${db.modelos[i].institucion}</strong></h5>
+            <h4 class="card-text">${db.modelos[i].nombre}</h4>
+            <h5 class="card-text"><i>Fecha: ${db.modelos[i].Fecha}</i></h5>
+            <p class="card-text" id="direccion">Dirección: ${db.modelos[i].locacion} </p>
+            <p class="card-text" id="linkIns"><a href="${db.modelos[i].linkInstagram}">Instagram </a></p>
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <!--- <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#ViewModal">Ver mas...</button> --->
+                </div>
+                <!-- <small class="text-body-secondary">9 mins</small> -->
+            </div>
+        </div>
+    </div>
+`;
+    post.innerHTML = postContent;
+    contenedorPosts.appendChild(post);
 }
-
-
+}
 
 /*
 const editCardBody = document.getElementById("editCardBody");
